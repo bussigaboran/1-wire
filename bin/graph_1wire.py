dce2cc6cc181393a92e1bf6b97709477f83c55ad
Anders Bornäs <anders@bornas.eu>
2022-10-24 22:14:06 +0200

    Py3 converted matplotlib functions

--- bin/graph_1wire.py
+++ bin/graph_1wire.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 """
-This program reads 1-wire sensors from and OWFS mounted file system.
-It stores the readings in an sqlite3 database
+Reads sensor data from sqlite3 database and saves a graph using matplotlib
 """
 
 import datetime
+import matplotlib
+import numpy as np
 import os
 import socket
 import sqlite3
 import time
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, dates
 
 sensor_path = '/mnt/1wire/'
-base_dir = '~/report/git/1-wire/'
-log_path = base_dir + 'log/'
-dbfile = os.path.expanduser(log_path + 'temperature.db')
+base_dir = os.path.expanduser('~/repos/git/1-wire/')
+dbfile = base_dir + 'log/temperature.db'
 
 
 def connect_to_database(fname):
@@ -34,100 +34,68 @@ def db_list_sensors(c):
 
 
 def current_time():
-    """ returns a string on the format 2020-06-11 00:00:00 
+    """ returns a string on the format 2020-06-11 14:23
         containing the current time """
     now = datetime.datetime.now()
-    time_string = now.strftime('%Y-%m-%d 00:00:00')
+    time_string = now.strftime('%Y-%m-%d %H:%M')
     return time_string
 
 
-def date_week_ago():
-    """ returns a time string with the date from 7 days ago 
-        on the format 2020-06-11 00:00:00. The time is set to
-        00:00:00 """
+def date_days_ago(no_days):
+    """ returns a time string with the date from <no_days> days ago
+        on the format 2020-06-11 14:23 """
     now = datetime.datetime.now()
-    week_ago = now - datetime.timedelta(days=7)
-    time_string = week_ago.strftime('%Y-%m-%d 00:00:00')
+    days_ago = now - datetime.timedelta(days=no_days)
+    time_string = days_ago.strftime('%Y-%m-%d %H:%M')
     return time_string
 
 
-def date_week_ago():
-    """ returns a time string with the date from 7 days ago
-        on the format 2020-06-11 00:00:00. The time is set to
-        00:00:00 """
-    now = datetime.datetime.now()
-    week_ago = now - datetime.timedelta(month=1)
-    time_string = week_ago.strftime('%Y-%m-%d 00:00:00')
-    return time_string
-
-
-def weekly_data(c, name, sensor_type):
-    week_ago = date_week_ago()
+def temperature_readings(name, c, days):
+    """ Function takes the following arguments:
+    - name: sensor name (location)
+    - c:    db cursor
+    - days: time period in days
+    It queries the database for all time/value pairs for that sensor going
+    back the specified number of days
+    """
     now = current_time()
-    # select * from sensors where name = 'garage' and timestamp
-    # BETWEEN '2020-06-14 0 9:52:00'  and '2020-06-14 10:00:00';
-    query = (f'select {sensor_type} from sensors where '
-             f'name = {name} and '
-             f'timestamp BETWEEN "{week_ago}" and "{now}"')
+    start_date = date_days_ago(days)
+    # select timestamp, temperature from sensors where
+    # name = "carport" and timestamp between
+    # "2022-09-8 18:12" and "2022-09-10 18:12"
+    query = (f'select timestamp, temperature from sensors where '
+             f'name = "{name}" and '
+             f'timestamp BETWEEN "{start_date}" and "{now}"')
     c.execute(query)
     rows = c.fetchall()
     return rows
 
 
-def weekly_data(c, name, sensor_type):
-    week_ago = date_week_ago()
-    now = current_time()
-    # select * from sensors where name = 'garage' and timestamp
-    # BETWEEN '2020-06-14 0 9:52:00'  and '2020-06-14 10:00:00';
-    query = (f'select {sensor_type} from sensors where '
-             f'name = {name} and '
-             f'timestamp BETWEEN "{week_ago}" and "{now}"')
-    c.execute(query)
-    rows = c.fetchall()
-    return rows
-
-
-def generate_graph(sensor_name, reading, time_period):
+def plot_temperature(sensor_name, c, days):
     """ Plot sensor data given the provided parameters
         sensor_name, reading, time_period """
-    fig = plt.figure()
-    graph = fig.add_subplot(111)
-    fig.suptitle(sensor_name)
-    fig.autofmt_xdate()
-    y1 = [value for (timestamp, value) in reading]
-    x0 = [timestamp for (timestamp, value) in reading]
-    x1 = matplotlib.dates.datestr2num(x0)
-    graph.plot_date(x=x1, y=y1, fmt="r-", label=sensor_name)
-    plt.ylabel("Temperature")
+    temp_data = temperature_readings(sensor_name, c, days)
+    date_format = "%Y-%m-%d %H:%M:%S"
+    y_data = np.array([float(value) for (timestamp, value) in temp_data])
+    x_time = np.array([datetime.datetime.strptime(timestamp, date_format) for (timestamp, value) in temp_data])
+    fig, ax = plt.subplots(1)
+    plt.xlabel("Time")
+    plt.ylabel("Temperature °C")
+    ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d %H:%M'))
+    ax.plot(x_time, y_data)
+    plt.gcf().autofmt_xdate()
+    plt.gca().set_ylim([0, None])
     if 'borken' in socket.gethostname():
-        fname = "/usr/share/nginx/www/tmp/%s.png" % sensor_name
+        fname = f'/usr/share/nginx/www/tmp/{sensor_name}_temp.png'
     else:
-        fname = base_dir + 'graphs/%s.png' % sensor_name
+        fname = f'{base_dir}graphs/{sensor_name}_temp.png'
     plt.savefig(fname, facecolor="#C2CEFF")
-
-    device = {}
-    sensor_id, sensor_type, name = sensor.split(':')
-    fhandle = open(sensor_path + sensor_id + '/' + sensor_type, 'r')
-    value = fhandle.read()
-    fhandle.close()
-    device['sensor_id'] = sensor_id
-    device['timestamp'] = time.strftime("%Y-%m-%d %H:%M:%S")
-    device['name'] = name
-    if sensor_type == 'temperature':
-        device['temperature'] = value
-    if sensor_type == 'humidity':
-        device['humidity'] = value
-    return device
 
 
 def main():
     db, c = connect_to_database(dbfile)
-    sensor_list = db_list_sensors(c)
-    print(weekly_data(c, 'carport', 'temperature'))
-    generate_graph("freezer", "temperature", "month")
-    #plot("garage", "temperature", "month")
-    #plot("carport", "week")
-    print(date_week_ago())
+    # sensor_list = db_list_sensors(c)
+    plot_temperature("carport", c, 1)
     db.close()
 
 
